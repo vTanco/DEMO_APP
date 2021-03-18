22986607dec687760e726d2f1e7b3178ea8786fa
Vicente <vicente.tanco@edu.uah.es>
2021-03-18 22:51:28 +0100

    init

---
+++ MAIN.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+import streamlit_metrics
+import os
+
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    layout="wide",
+    initial_sidebar_state="expanded")
+st.title('Demo app')
+
